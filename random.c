2fc6e940b05c1812d7124e985c849ab2470ec377
Isack-M <mayorga11377@gmail.com>
2025-08-27 12:42:49 -0400

    Fixed mistake
    Replaced comma with semi colon and added a null character at the end of the random string

--- random.c
+++ random.c
@@ -3,8 +3,9 @@
 
 void rand_string(char *s, size_t size){
     char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    for(size_t i = 0; i< size, i++){
+    for(size_t i = 0; i< size; i++){
         int key = rand() % (sizeof(charset) - 1);
         s[i] = charset[key];
     }
+    s[size] = '\0';
 }
