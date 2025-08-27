a4cc367fa3f2293f7f1c722160c24e91216ca9b9
Isack-M <mayorga11377@gmail.com>
2025-08-27 12:37:19 -0400

    Update random.c
    for loop that loops through the character set, takes a random char and adds it to the new random string

--- random.c
+++ random.c
@@ -3,4 +3,8 @@
 
 void rand_string(char *s, size_t size){
     char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    for(size_t i = 0; i< size, i++){
+        int key = rand() % (sizeof(charset) - 1);
+        s[i] = charset[key];
+    }
 }
